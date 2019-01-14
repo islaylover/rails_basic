5ed31da9f2eacdc2b39b00a181667fc9ef053f46
macwin <cinema.taro.ex@gmail.com>
2019-01-14 22:24:18 +0900

    Add docker files and add explanation in order to set up development environment <Ruby on Rails and Mysql>

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2018_11_16_120802) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", limit: 30
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
